8f456bef52e678b5898290bb511fcae8eb2b55a2
Shithappe <greenmad.b50@gmail.com>
2023-09-28 00:27:05 +0300

    set db

---
+++ database/migrations/2023_09_27_212048_create_records_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('time');
+            $table->string('username');
+            $table->string('phone');
+
+            $table->unsignedBigInteger('salon_id');
+            $table->foreign('salon_id')
+                  ->references('id')->on('salons')
+                  ->onDelete('cascade');
+
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')
+                ->references('id')->on('services')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
